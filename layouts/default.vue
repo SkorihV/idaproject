b80f839a6ecba2062b8941b4955618bbf4cf67e0
SkorihV <skorih.vitalii@gmail.com>
2022-07-25 16:04:56 +0700

    Базовая конфигурация проекта

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h1>Заголовок</h1>
+    <nuxt />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  color: $test;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, vero!</p>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
